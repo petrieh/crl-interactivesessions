15e8907ab11b3b7ac1ed1cc29e03b6eebcb7f155
Huovinen, Petri Juhani <petri.huovinen@nokia.com>
2019-07-18 09:33:56 +0300

    Correct pickle.dumps in runnerterminal

--- src/crl/interactivesessions/runnerterminal.py
+++ src/crl/interactivesessions/runnerterminal.py
@@ -21,7 +21,7 @@ from crl.interactivesessions.runnerexceptions import (
     RemoteTimeout,
     remotetimeouthandler)
 from .garbagemanager import GarbageManager
-
+from .shells.remotemodules.compatibility import PY3
 
 __copyright__ = 'Copyright (C) 2019, Nokia'
 
@@ -625,8 +625,10 @@ class RunnerTerminal(object):
         self.run("_handlercode = compile({handler_content}, "
                  "'RunnerHandler', 'exec')".format(
                      handler_content=(
-                         "pickle.loads(b{0!r})".format(pickle.dumps(
-                             handler_content, protocol=0), protocol=0))))
+                         "pickle.loads({b}{dumps!r})".format(
+                             b='' if PY3 else 'b',
+                             dumps=pickle.dumps(
+                                 handler_content, protocol=0)))))
         self.run('runnerhandlerns = {}')
         self.run("exec(_handlercode, runnerhandlerns)")
 
